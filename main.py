e943b3741cdb398075619b0fa5290aa16d3709e6
darkress <github@darkress.xyz>
2022-05-22 01:57:17 +0200

    Increased efficiency

--- main.py
+++ main.py
@@ -5,15 +5,13 @@ import sys
 
 HOST = "flood.schenklflut.de"
 PORT = 1234
-offsetx = 1000
-offsety = 800
+offsetx = 1820
+offsety = 0
 
 
 def main():
 
     imagename = str(sys.argv[1]) # Adjust this to the name of the iamge
-
-
 
     # Create a TCP/IP socket
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -26,39 +24,34 @@ def main():
     datastream = sock.recv(1024)
     print(datastream.decode())
 
-
-
-
     im = Image.open(imagename)  # Can be many different formats.
     pix = im.load()
-    size = im.size
-    print(size) #Absolutey not necessary only here for fucking testing shit
     x, y = im.size #Split the shit into x,y values
-    print(x)
-    print(y)
-    #print(pix[500, 500])
-
     #Parse image into more convenient data type
 
-    pixelarray = [[0] * y for _ in range(x)]
+    pixelarray = [""]
 
+    count = 0
 
     for i in range(x):
         for j in range(y):
 
             r, g, b, t = pix[i, j]
-            pixelarray[i][j] = '{:02x}{:02x}{:02x}'.format(r, g, b)
+            if t != 0:
+                count += 1
+                pixelarray.append("PX " + str(i+offsetx) + " " + str(j+offsety) + " " + '{:02x}{:02x}{:02x}'.format(r, g, b) + "\n")
+                #print(pixelarray[1])
 
     #Do the actual dump to the server
 
+
     while True:
 
-        for i in range(x):
-            for j in range(y):
+        for i in range(count):
 
-                sendbuffer = "PX " + str(i+offsetx) + " " + str(j+offsety) + " " + str(pixelarray[i][j]) + "\n"
-                sock.send(sendbuffer.encode())
-                print(sendbuffer)
+            sock.send(pixelarray[i].encode())
+            print(pixelarray[i])
+
 
     sock.close()
 
