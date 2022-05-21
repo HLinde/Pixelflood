fef2da472e686a5fe3e6b712a78797110084550d
darkress <github@darkress.xyz>
2022-05-21 18:16:48 +0200

    Working state

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from PIL import Image
 
 def main():
 
+    imagename = "r37sjR-ohjo.jpg"
+
     HOST = "flood.schenklflut.de"
     PORT = 1234
 
@@ -18,21 +20,28 @@ def main():
     datastream = sock.recv(1024)
     print(datastream.decode())
 
-    sock.close()
 
 
 
-    im = Image.open('uc.png')  # Can be many different formats.
+    im = Image.open(imagename)  # Can be many different formats.
     pix = im.load()
     size = im.size
     print(size) #Absolutey not necessary only here for fucking testing shit
     x, y = im.size #Split the shit into x,y values
     print(x)
     print(y)
-    print(pix[500, 500])
+    #print(pix[500, 500])
 
     for x in range(x):
         for y in range(y):
+
             print(pix[x, y])
+            r, g, b = pix[x, y]
+            #print("PX " + str(x) + " " + str(y)+ " " + str(format(r, 'x')) + str(format(g, 'x')) + str(format(b, 'x')))
+            shittosend = "PX " + str(x) + " " + str(y) + " " + str(format(r, 'x')) + str(format(g, 'x')) + str(format(b, 'x'))
+            sock.send(shittosend.encode())
+
+
+    sock.close()
 
 main()
